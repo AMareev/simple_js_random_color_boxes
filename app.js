54875cfe17fed1cce6bfc20d2ec5a2761f3d1f68
AMareev <alexandr.mareev@gmail.com>
2023-03-10 19:49:51 +0300

    init

---
+++ app.js
@@ -0,0 +1,29 @@
+const board = document.querySelector('#board');
+const colors = ['#c7a6e2', '#d986d2', '#e2b5a7', '#dfdfa2', '#e8e8c4']
+const SQUARE_NUMBERS = 500;
+
+for (let i = 0; i < SQUARE_NUMBERS; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+
+    square.addEventListener('mouseleave', () => removeColor(square))
+
+    board.append(square)
+}
+
+function setColor(e) {
+    e.style.backgroundColor= getRandomColor()
+    e.style.boxShadow = `0 0 2px ${e.style.backgroundColor}, 0 0 10px ${e.style.backgroundColor}`
+}
+
+function removeColor(e) {
+    e.style.backgroundColor= '#1d1d1d'
+    e.style.boxShadow = null
+}
+
+function getRandomColor() {
+    const index=  Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
